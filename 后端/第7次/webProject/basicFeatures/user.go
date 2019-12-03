3d1e9771a945ec934135ff2b1e0342cdf694b66d
zzzzzip <zzzzip6@gmail.com>
2019-12-03 23:43:08 +0800

    增加发布留言和查看留言功能

--- 后端/第7次/webProject/basicFeatures/user.go
+++ 后端/第7次/webProject/basicFeatures/user.go
@@ -5,10 +5,10 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
-
+	"time"
 )
 
-func Login(db *sql.DB, c *gin.Context) {
+func Login(db *sql.DB, c *gin.Context) string {
 	username := c.PostForm("username")
 	password := c.DefaultPostForm("password", "admin")
 	if IsRegiste(db, "student", username) {
@@ -21,7 +21,8 @@ func Login(db *sql.DB, c *gin.Context) {
 		} else {
 			_, passwd := SelectDatabase(db, "student", username)
 			if passwd == password {
-				c.SetCookie(username, "loginSucessedly!", 2, "/", "127.0.0.1", false, true)
+				//暂时不支持中文cookie，如果用户名是中文，则会报错
+				c.SetCookie(username, "loginSucessedly!", 100, "/", "127.0.0.1", false, true)
 				c.JSON(http.StatusOK, gin.H{
 					"status":  http.StatusOK,
 					"message": "登录成功！",
@@ -39,6 +40,7 @@ func Login(db *sql.DB, c *gin.Context) {
 			"message": "账号未注册！",
 		})
 	}
+	return username
 }
 
 func Registe(db *sql.DB, c *gin.Context) {
@@ -110,4 +112,64 @@ func SelectDatabase(db *sql.DB, tableName string, username string) (name string,
 		stmt.Scan(&id, &name, &passwd)
 	}
 	return name, passwd
+}
+
+func SendMsg(db *sql.DB, c *gin.Context, username string) {
+	_, err := c.Cookie(username)
+	//CheckError(err)
+	if err == nil {
+		message := c.PostForm("message")
+		pid1 := c.DefaultPostForm("pid", "0")
+		timeNow := time.Now().Format("2006-01-02/03:04:05")
+
+		stmt, err := db.Prepare("insert into message(pid,username,message,time) values(?, ?, ?, ?)")
+		CheckError(err)
+		stmt.Exec(pid1, username, message, timeNow)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "发送成功！",
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status": http.StatusOK,
+			"error":  "没有登录，不能发送消息！",
+		})
+	}
+}
+
+func GetMsg(db *sql.DB, c *gin.Context) {
+	var pid, id int
+	var username, message, time string
+
+	//_, err := c.Cookie(username)
+	toId := c.DefaultPostForm("pid", "0")
+	stmt, err := db.Query("select * from message where pid=" + toId)
+	CheckError(err)
+	for stmt.Next() {
+		stmt.Scan(&id, &pid, &username, &message, &time)
+		c.JSON(http.StatusOK, gin.H{
+			"time":     time,
+			"username": username,
+			"message":  message,
+		})
+	}
+}
+
+func WebServerStart(db *sql.DB)  {
+	var username string
+	r := gin.Default()
+
+	r.POST("/registe", func(c *gin.Context) {
+		Registe(db, c)
+	})
+	r.POST("/login", func(c *gin.Context) {
+		username = Login(db, c)
+	})
+	r.POST("/login/sendMsg", func(c *gin.Context) {
+		SendMsg(db, c, username)
+	})
+	r.POST("/login/getMsg", func(c *gin.Context) {
+		GetMsg(db, c)
+	})
+	r.Run()
 }
--- 后端/第7次/webProject/main.go
+++ 后端/第7次/webProject/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"webProject/basicFeatures"
 )
@@ -9,15 +8,5 @@ import (
 func main() {
 	db := basicFeatures.OpenDatabase("root", "mima", "chatBord")
 	defer db.Close()
-	r := gin.Default()
-
-	r.POST("/registe", func(c *gin.Context) {
-		basicFeatures.Registe(db, c)
-	})
-	r.POST("/login", func(c *gin.Context) {
-		basicFeatures.Login(db, c)
-	})
-
-	r.Run()
-
+	basicFeatures.WebServerStart(db)
 }
